6c6b018da3128767810b04970b6dfde729771dd8
joopitz <jonas.opitz@live.de>
2023-06-08 15:27:41 +0200

    feat: initial implementation, including text statistics

---
+++ src/python_ki_hydra/main.py
@@ -0,0 +1,20 @@
+import cherrypy
+import python_ki_hydra.text_statistics as text_statistics
+
+
+class WebService:
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out()
+    def analyze_text(self):
+        return text_statistics.analyze_text(cherrypy.request.json)
+
+
+def main():
+    # listen to requests from any incoming IP address
+    cherrypy.server.socket_host = "0.0.0.0"
+    cherrypy.quickstart(WebService())
+
+
+if __name__ == "__main__":
+    main()
