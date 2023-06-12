4f709fa647b9537cb436aabc1db996415b0ed33c
joopitz <jonas.opitz@live.de>
2023-06-12 10:22:58 +0200

    minor(build): fix tag of nix docker image used for build

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 docker run \
        --rm \
        -v .:/python_kidra \
-       docker.nix-community.org/nixpkgs/nix-flakes \
+       docker.nix-community.org/nixpkgs/nix-flakes:nixos-22.11 \
        sh /python_kidra/scripts/build_docker_using_docker.sh
