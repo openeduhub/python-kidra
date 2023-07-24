dca456fb63320f1dda45d9e1651c7d8259431880
joopitz <jonas.opitz@live.de>
2023-07-24 09:03:49 +0200

    feat: make bootstrapped nix builder in docker persistent
    This avoids needlessly downloading and building all dependencies any time a
    build is run.

--- scripts/build_docker_using_docker.sh
+++ scripts/build_docker_using_docker.sh
@@ -1,8 +1,6 @@
 #!/bin/sh 
 echo "sandbox = false" >> /etc/nix/nix.conf &&
     cd /python_kidra &&
-    nix build --out-link result-link
-
-rm result
-cp $(readlink result-link) result
-rm result-link
+    nix build --out-link result-link &&
+    cp -f $(readlink result-link) result &&
+    rm result-link
