6c14e5b7f381a3ae488330f81b46a9c6eecd92bc
joopitz <jonas.opitz@live.de>
2023-07-24 09:03:49 +0200

    feat: make bootstrapped nix builder in docker persistent
    This avoids needlessly downloading and building all dependencies any time a
    build is run.

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-docker run \
-       --rm \
-       -v .:/python_kidra \
-       docker.nix-community.org/nixpkgs/nix-flakes:nixos-22.11 \
+# create a docker container that hosts the nix-based builder.
+# make it persistent so that future build tasks do not start from scratch again
+docker run -d \
+       -it \
+       --mount src="$(pwd)",target=/python_kidra,type=bind \
+       --name kidra-builder \
+       docker.io/nixpkgs/nix-flakes:nixos-23.05
+
+# execute the build command
+docker exec kidra-builder \
        sh /python_kidra/scripts/build_docker_using_docker.sh
