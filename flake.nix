d45321f47dffc2f2341c6fa21d2a9a514e6bbfdb
joopitz <jonas.opitz@live.de>
2023-07-28 15:04:44 +0200

    !refactor: run sub-services as individual, isolated applications
    BREAKING CHANGES:
    - sub-domain for the text-statistics service:
      analyze-text -> text-statistics/
      (Note the trailing forward-slash -- it is not optional)
    
    Before, sub-services were accessed as Python packages within the kidra's Python
    environment. This can cause various dependency issues, as each sub-service's
    dependencies will also be pulled into the kidra's local environment. Thus, all
    sub-services were required to run on a unified environment.
    
    Now, each sub-service's dependencies can be fully isolated, thus allowing us to
    run multiple different versions of the same dependency, if necessary.
    Note that this may increase the size of the application considerably, as few
    dependencies will be shared. However, we can still optionally unify the
    versions of individual sub-service's environments in order to save space.

--- flake.nix
+++ flake.nix
@@ -4,26 +4,26 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    text_statistics = {
-      url = "github:openeduhub/text-statistics";
-      inputs.nixpkgs.follows = "nixpkgs";
+    text-statistics = {
+      url = "github:openeduhub/text-statistics/native-application";
+      # inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        
-        pkgs = nixpkgs.legacyPackages.${system};
-        text_statistics = self.inputs.text_statistics.defaultPackage.${system};
+        pkgs = import nixpkgs {inherit system;};
+        python = pkgs.python310;
+        text-statistics = self.inputs.text-statistics.packages.${system}.text-statistics;
 
         # declare the python packages used for building & developing
         python-packages-build = python-packages:
           with python-packages; [
             cherrypy
-            text_statistics
+            requests
           ];
-        python-build = pkgs.python3.withPackages python-packages-build;
+        python-build = python.withPackages python-packages-build;
 
 
         python-packages-devel = python-packages:
@@ -32,60 +32,49 @@
             pyflakes
             isort
             ipython
+            jupyter
           ] ++ (python-packages-build python-packages);
-        python-devel = pkgs.python3.withPackages python-packages-devel;
+        python-devel = python.withPackages python-packages-devel;
 
         # declare, how the python application shall be built
-        python_kidra = python-build.pkgs.buildPythonApplication {
-            pname = "python_kidra";
-            version = "1.0.1";
-
-            propagatedBuildInputs =
-              (python-packages-build python-build.pkgs);
-
+        python-kidra = python-build.pkgs.buildPythonApplication {
+            pname = "python-kidra";
+            version = "1.1.0";
+            # we have to disable catching conflicts here because
+            # it is giving false positives due to the import of other python
+            # packages that are not actually part of the python environment
+            # for the kidra
+            catchConflicts = false;
+            propagatedBuildInputs = [
+              python-build
+              text-statistics
+            ];
             src = ./.;
           };
 
-        # download nltk-punkt, an external requirement for nltk
-        nltk-punkt = pkgs.fetchurl {
-          url = "https://github.com/nltk/nltk_data/raw/5db857e6f7df11eabb5e5665836db9ec8df07e28/packages/tokenizers/punkt.zip";
-          sha256 = "sha256-UcMHiZSur2UL/I4Ci+T7QrSg0XfUHAErapg5eWU2YOw=";
-        };
-
         # declare, how the docker image shall be built
-        docker-image = pkgs.dockerTools.buildImage {
-          name = "python-kidra";
-          tag = python_kidra.version;
-
-          # unzip nltk-punkt and put it into a directory that nltk considers
+        docker-img = pkgs.dockerTools.buildImage {
+          name = python-kidra.pname;
+          tag = python-kidra.version;
           config = {
-            Cmd = [
-              "${pkgs.bash}/bin/sh"
-              (pkgs.writeShellScript "runDocker.sh" ''
-                ${pkgs.coreutils}/bin/mkdir -p /nltk_data/tokenizers;
-                ${pkgs.unzip}/bin/unzip ${nltk-punkt} -d /nltk_data/tokenizers;
-                /bin/python_kidra
-              '')
-            ];
-            WorkingDir = "/";
-          };
-
-          # copy the binary of the application into the image
-          copyToRoot = pkgs.buildEnv {
-            name = "image-root";
-            paths = [ python_kidra ];
-            pathsToLink = [ "/bin" ];
+            Cmd = [ "${python-kidra}/bin/python-kidra" ];
           };
         };
 
       in {
-        defaultPackage = docker-image;
-        devShell = pkgs.mkShell {
+        packages = rec {
+          inherit python-kidra;
+          docker = docker-img;
+          default = python-kidra;
+        };
+        devShells.default = pkgs.mkShell {
           buildInputs = [
-            text_statistics
+            text-statistics
             python-devel
             # python language server
             pkgs.nodePackages.pyright
+            # nix language server
+            pkgs.rnix-lsp
           ];
         };
       }
