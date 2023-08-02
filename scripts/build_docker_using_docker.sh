3c2991dc02e4f25f45c82a06ac8aeb29d7b52f2e
joopitz <jonas.opitz@gwdg.de>
2023-08-02 11:35:09 +0200

    fix(build): bootstrapped builder building the wrong thing

--- scripts/build_docker_using_docker.sh
+++ scripts/build_docker_using_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh 
 echo "sandbox = false" >> /etc/nix/nix.conf &&
     cd /python_kidra &&
-    nix build --out-link result-link &&
+    nix build .\#docker --out-link result-link &&
     cp -f $(readlink result-link) result &&
     rm result-link
