264c93aec585c025b7fa2610a6fbb741af1458c5
joopitz <jonas.opitz@live.de>
2023-08-03 11:43:03 +0200

    feat(nix): make wlo-topic-assistant available to kidra

--- flake.nix
+++ flake.nix
@@ -3,12 +3,34 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    # utilities
     flake-utils.url = "github:numtide/flake-utils";
+    nix-filter.url = "github:numtide/nix-filter";
+    # sub-services
     text-statistics = {
       url = "github:openeduhub/text-statistics/native-application";
-      # inputs.nixpkgs.follows = "nixpkgs";
+      inputs = {
+        /*
+        override inputs to follow ours.
+        while this causes the resulting package / image to be much smaller,
+        this setting is risky and may cause breaks later, as we are now
+        controlling the package versions of this sub-service here, rather than
+        in the service itself.
+        as a result, updates / changes to the package versions in the service
+        will not actually affect anything for the service run within the kidra.
+        */    
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
     };
-    nix-filter.url = "github:numtide/nix-filter";
+    wlo-topic-assistant = {
+      url = "github:joopitz/wlo-topic-assistant/plainNix";
+      # see comment above
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
@@ -16,8 +38,11 @@
       let
         pkgs = import nixpkgs {
           inherit system;
-          # add text-statistics to our collection of nix packages
-          overlays = [self.inputs.text-statistics.overlays.default];
+          # add overlays of the sub-services
+          overlays = [
+            self.inputs.text-statistics.overlays.default
+            self.inputs.wlo-topic-assistant.overlays.default
+          ];
         };
         # an alias for the python version we are using
         python = pkgs.python310;
@@ -42,7 +67,7 @@
         ### declare how the python application shall be built
         python-kidra = python.pkgs.buildPythonApplication rec {
           pname = "python-kidra";
-          version = "1.1.0";
+          version = "1.1.1";
           src = nix-filter {
             root = self;
             include = [
@@ -54,14 +79,15 @@
           };
           propagatedBuildInputs = (python-packages-build python.pkgs);
           /*
-          only make available the binary of text-statistics to the kidra.
-          if we simply included the entire package,
-          its propagated dependencies (i.e. python libraries) would also be
+          only make available the binaries of the sub-services to the kidra.
+          if we simply included the entire packages,
+          their propagated dependencies (i.e. python libraries) would also be
           included in the environment of the kidra, breaking isolation and
           likely causing version conflicts.
           */
           makeWrapperArgs = [
             "--prefix PATH : ${pkgs.lib.makeBinPath [pkgs.text-statistics]}"
+            "--prefix PATH : ${pkgs.lib.makeBinPath [pkgs.wlo-topic-assistant]}"
           ];
         };
 
