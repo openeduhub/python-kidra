4766be89d08e42ed2f12adbc3fda785f7f08ac03
jonas.opitz <jonas.opitz@gwdg.de>
2023-08-28 12:28:10 +0200

    bump(nix-openapi-checks): automatically test endpoints
    Skip the endpoints /link-wikipedia and /text-extraction,
    since they require access to the internet.

--- flake.nix
+++ flake.nix
@@ -20,15 +20,14 @@
     text-statistics = {
       url = "github:openeduhub/text-statistics";
       inputs = {
-        /*
-        override inputs to follow ours.
-        while this causes the resulting package / image to be much smaller,
-        this setting is risky and may cause breaks later, as we are now
-        controlling the package versions of this sub-service here, rather than
-        in the service itself.
-        as a result, updates / changes to the package versions in the service
-        will not actually affect anything for the service run within the kidra.
-        */    
+        /* override inputs to follow ours.
+           while this causes the resulting package / image to be much smaller,
+           this setting is risky and may cause breaks later, as we are now
+           controlling the package versions of this sub-service here, rather than
+           in the service itself.
+           as a result, updates / changes to the package versions in the service
+           will not actually affect anything for the service run within the kidra.
+        */
         nixpkgs.follows = "nixpkgs";
         flake-utils.follows = "flake-utils";
         openapi-checks.follows = "openapi-checks";
@@ -74,30 +73,28 @@
           ];
         };
         # swagger-cli is only available in nixpkgs unstable
-        pkgs-unstable = import nixpkgs-unstable {inherit system;};
+        pkgs-unstable = import nixpkgs-unstable { inherit system; };
         # an alias for the python version we are using
         python = pkgs.python310;
 
-        nix2container = self.inputs.nix2container.packages.${system}.nix2container;
+        nix2container =
+          self.inputs.nix2container.packages.${system}.nix2container;
         # utility to easily filter out unnecessary files from the source
         nix-filter = self.inputs.nix-filter.lib;
         openapi-checks = self.inputs.openapi-checks.lib.${system};
 
         ### declare the python packages used for building & developing
         python-packages-build = python-packages:
-          with python-packages; [ fastapi
-                                  pydantic
-                                  uvicorn
-                                  requests
-                                ];
-        
+          with python-packages; [
+            fastapi
+            pydantic
+            uvicorn
+            requests
+          ];
+
         python-packages-devel = python-packages:
-          with python-packages; [ black
-                                  pyflakes
-                                  isort
-                                  ipython
-                                  jupyter
-                                ]
+          with python-packages;
+          [ black pyflakes isort ipython jupyter ]
           ++ (python-packages-build python-packages);
 
         ### declare how the python application shall be built
@@ -106,30 +103,26 @@
           version = "1.1.3";
           src = nix-filter {
             root = self;
-            include = [
-              "src"
-              ./setup.py
-              ./requirements.txt
-            ];
+            include = [ "src" ./setup.py ./requirements.txt ];
             exclude = [ (nix-filter.matchExt "pyc") ];
           };
           propagatedBuildInputs = (python-packages-build python.pkgs);
-          /*
-          only make available the binaries of the sub-services to the kidra.
-          if we simply included the entire packages,
-          their propagated dependencies (i.e. python libraries) would also be
-          included in the environment of the kidra, breaking isolation and
-          likely causing version conflicts.
+          /* only make available the binaries of the sub-services to the kidra.
+             if we simply included the entire packages,
+             their propagated dependencies (i.e. python libraries) would also be
+             included in the environment of the kidra, breaking isolation and
+             likely causing version conflicts.
           */
-          makeWrapperArgs =
-            [ "--suffix PATH : ${
-              pkgs.lib.makeBinPath
-                [ pkgs.text-statistics
-                  pkgs.text-extraction
-                  pkgs.wlo-topic-assistant
-                  pkgs.wlo-classification
-                ]}"
-            ];
+          makeWrapperArgs = [
+            "--suffix PATH : ${
+              pkgs.lib.makeBinPath [
+                pkgs.text-statistics
+                pkgs.text-extraction
+                pkgs.wlo-topic-assistant
+                pkgs.wlo-classification
+              ]
+            }"
+          ];
         };
 
         ### declare how the docker image shall be built
@@ -138,22 +131,21 @@
           tag = python-kidra.version;
           config = {
             Cmd = [ "${python-kidra}/bin/python-kidra" ];
-            ExposedPorts = {
-              "8080/tcp" = {};
-            };
+            ExposedPorts = { "8080/tcp" = { }; };
           };
-          layers =
-            (map
-              (pkg: nix2container.buildLayer {deps = [pkg]; maxLayers = 20;})
-              [ pkgs.text-statistics
-                pkgs.text-extraction
-                pkgs.wlo-topic-assistant
-                pkgs.wlo-classification
-              ]);
+          layers = (map
+            (pkg: nix2container.buildLayer { deps = [ pkg ]; maxLayers = 20; })
+            [
+              pkgs.text-statistics
+              pkgs.text-extraction
+              pkgs.wlo-topic-assistant
+              pkgs.wlo-classification
+            ]);
           maxLayers = 20;
         };
 
-      in {
+      in
+      {
         packages = rec {
           inherit python-kidra;
           docker = docker-img;
@@ -169,12 +161,12 @@
           ];
         };
         checks = {
-          openapi-check = openapi-checks.openapi-valid {
-            serviceBin = "${self.packages.${system}.python-kidra}/bin/python-kidra";
-            openapiDomain = "v3/api-docs";
-            memorySize = 6144;
+          test-service = openapi-checks.test-service {
+            service-bin = "${python-kidra}/bin/python-kidra";
+            openapi-domain = "v3/api-docs";
+            memory-size = 6144;
+            skip-endpoints = [ "/link-wikipedia" "/text-extraction" ];
           };
         };
-      }
-    );
+      });
 }
